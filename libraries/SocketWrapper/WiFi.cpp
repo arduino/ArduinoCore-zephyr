76a31522a4eccfee2a073f159c2f891595ae9ae7
Bogdan Ivanus <b.ivanus@arduino.cc>
2025-09-10 17:59:17 +0300

    ZEP-55: Implemented WiFi.scan() functionality.

--- libraries/SocketWrapper/WiFi.cpp
+++ libraries/SocketWrapper/WiFi.cpp
@@ -1,3 +1,5 @@
 #include "WiFi.h"
 
 WiFiClass WiFi;
+
+WiFiClass* WiFiClass::instance = nullptr;
--- libraries/SocketWrapper/WiFi.h
+++ libraries/SocketWrapper/WiFi.h
@@ -3,6 +3,9 @@
 #include "utility/wl_definitions.h"
 #include <zephyr/net/wifi_mgmt.h>
 
+// Max number of scan results to store
+#define MAX_SCAN_RESULTS 20
+
 #define NET_EVENT_WIFI_MASK                                                                        \
 	(NET_EVENT_WIFI_CONNECT_RESULT | NET_EVENT_WIFI_DISCONNECT_RESULT |                        \
 	 NET_EVENT_WIFI_AP_ENABLE_RESULT | NET_EVENT_WIFI_AP_DISABLE_RESULT |                      \
@@ -23,22 +26,28 @@ public:
         sta_config.ssid_length = strlen(ssid);
         sta_config.psk = (const uint8_t *)passphrase;
         sta_config.psk_length = strlen(passphrase);
-        // TODO: change these fields with scan() results
-        sta_config.security = WIFI_SECURITY_TYPE_PSK;
-        sta_config.channel = WIFI_CHANNEL_ANY;
-        sta_config.band = WIFI_FREQ_BAND_2_4_GHZ;
-	    sta_config.bandwidth = WIFI_FREQ_BANDWIDTH_20MHZ;
 
-        int ret = net_mgmt(NET_REQUEST_WIFI_CONNECT, sta_iface, &sta_config,
-                sizeof(struct wifi_connect_req_params));
-        if (ret) {
-            return false;
-        }
+       	// Register the Wi-Fi event callback
+	    net_mgmt_init_event_callback(&wifiCb, scanEventDispatcher, NET_EVENT_WIFI_SCAN_RESULT | NET_EVENT_WIFI_SCAN_DONE);
 
-        NetworkInterface::begin(false, NET_EVENT_WIFI_MASK);
-        if (blocking) {
-            net_mgmt_event_wait_on_iface(sta_iface, NET_EVENT_WIFI_CONNECT_RESULT, NULL, NULL, NULL, K_FOREVER);
-        }
+	    net_mgmt_add_event_callback(&wifiCb);
+
+	   	(void)scanNetworks();
+
+	   	// Check if the network we were seekin was found and attempt to connect to it
+	    if(getSoughtNetworkFound() != true)
+	    {
+	        int ret = net_mgmt(NET_REQUEST_WIFI_CONNECT, sta_iface, &sta_config,
+	                sizeof(struct wifi_connect_req_params));
+	        if (ret) {
+	            return false;
+	        }
+
+	        NetworkInterface::begin(false, NET_EVENT_WIFI_MASK);
+	        if (blocking) {
+	            net_mgmt_event_wait_on_iface(sta_iface, NET_EVENT_WIFI_CONNECT_RESULT, NULL, NULL, NULL, K_FOREVER);
+	        }
+	    }
 
         return status();
     }
@@ -90,8 +99,18 @@ public:
         return WL_NO_SHIELD;
     }
 
-    int8_t scanNetworks() {
-        // TODO: borrow code from mbed core for scan results handling
+    uint8_t scanNetworks() {
+    	resultCount = 0u;
+    	setScanSequenceFinished(false);
+    	setSoughtNetworkFound(false);
+
+    	// Trigger a new scan
+        net_mgmt(NET_REQUEST_WIFI_SCAN, sta_iface, nullptr, 0u);
+
+        // Wait for the scan to finish. This is by design a blocking call
+        while(getScanSequenceFinished() != true);
+
+        return resultCount;
     }
 
     char* SSID() {
@@ -108,6 +127,66 @@ public:
         return 0;
     }
 
+	static void scanEventDispatcher(struct net_mgmt_event_callback *cb, uint64_t mgmt_event, struct net_if *iface)
+	{
+		if (instance != nullptr)
+		{
+			instance->handleScanEvent(cb, mgmt_event, iface);
+		}
+	}
+
+    void handleScanEvent(struct net_mgmt_event_callback *cb, uint64_t mgmt_event, struct net_if *iface) {
+		    if (mgmt_event == NET_EVENT_WIFI_SCAN_RESULT) {
+		       	const struct wifi_scan_result *entry = reinterpret_cast<const struct wifi_scan_result *>(cb->info);
+		        if (resultCount < MAX_SCAN_RESULTS) {
+		            memcpy(&scanResults[resultCount], entry, sizeof(struct wifi_scan_result));
+		            resultCount++;
+
+		            //for each new result found, compare network name with desired one
+		            if(!memcmp(entry->ssid, sta_config.ssid, entry->ssid_length))
+		            {
+		            	// if a match is found, add missing info to config before attempting to connect
+		        		sta_config.security = entry->security;
+		        		sta_config.channel = entry->channel;
+		        		sta_config.band = entry->band;
+		        		sta_config.bandwidth = WIFI_FREQ_BANDWIDTH_20MHZ;
+
+		        		setSoughtNetworkFound(true);
+		            }
+		        }
+		    }
+
+		    if (mgmt_event == NET_EVENT_WIFI_SCAN_DONE) {
+		    	setScanSequenceFinished(true);
+
+		        if (resultCount = 0) {
+		        	printk("No networks found.\n");
+		        }
+		    }
+		}
+
+		void setScanSequenceFinished(bool scanFinished)
+		{
+			scanSequenceFinished = scanFinished;
+		}
+
+		void setSoughtNetworkFound(bool networkFound)
+		{
+			soughtNetworkFound = networkFound;
+		}
+
+		bool getScanSequenceFinished(void)
+		{
+			return scanSequenceFinished;
+		}
+
+		bool getSoughtNetworkFound(void)
+		{
+			return soughtNetworkFound;
+		}
+
+		static WiFiClass* instance;
+
 private:
     struct net_if *sta_iface = nullptr;
     struct net_if *ap_iface = nullptr;
@@ -116,6 +195,13 @@ private:
     struct wifi_connect_req_params sta_config;
 
     struct wifi_iface_status sta_state = { 0 };
+
+    struct wifi_scan_result scanResults[MAX_SCAN_RESULTS];
+	uint8_t resultCount;
+	struct net_mgmt_event_callback wifiCb;
+
+	bool soughtNetworkFound = false;
+	bool scanSequenceFinished = false;
 };
 
 extern WiFiClass WiFi;
