c3b16a084fbd7a7410c19d2d1b8944d62271f15b
Gilberto Conti <g.conti@arduino.cc>
2026-05-18 16:32:12 +0200

    loader: add ota support
    On boot, install /ota:/UPDATE_SKETCH.BIN into the user_sketch partition
    when the file is present. Validates the inner sketch header and bounds,
    and keeps UPDATE_SKETCH.BIN on post-erase failure so the next boot can retry.
    
    Signed-off-by: Gilberto Conti <g.conti@arduino.cc>

---
+++ cores/arduino/ota_api.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) Arduino s.r.l. and/or its affiliated companies
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifdef CONFIG_OTA
+
+#include "../../loader/ota/ota_api.h"
+
+#endif // CONFIG_OTA
