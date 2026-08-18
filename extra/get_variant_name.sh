f3126ad55b1487982dfde5c8d6e035897d47526c
Andrea Gilardoni <a.gilardoni@arduino.cc>
2026-08-18 14:20:31 +0200

    extra: scripts: improving python venv usage
    Add the possibility of skipping venv activation and allow venv path to
    be provided through PYTHON_VENV_PATH env variable

--- extra/get_variant_name.sh
+++ extra/get_variant_name.sh
@@ -5,7 +5,8 @@
 
 set -e
 
-source venv/bin/activate
+PYTHON_VENV_PATH=${PYTHON_VENV_PATH:-"./venv"}
+[[ -z "${DISABLE_VENV:-}" ]] && { source ${PYTHON_VENV_PATH}/bin/activate; }
 
 # Get the variant name (NORMALIZED_BOARD_TARGET in Zephyr)
 variant=$(cmake "-DBOARD=$1" -P extra/get_variant_name.cmake 2>/dev/null | grep 'VARIANT=' | cut -d '=' -f 2)
