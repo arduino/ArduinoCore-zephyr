3f1fce60a16dede4aba9195432f148d367203e90
pennam <m.pennasilico@arduino.cc>
2026-09-21 15:38:27 +0200

    variants: nano_33_ble_sense: fix legacy SPI/I2C defines and add RGB LED defines

--- variants/arduino_nano_33_ble_nrf52840_sense/variant.h
+++ variants/arduino_nano_33_ble_nrf52840_sense/variant.h
@@ -4,13 +4,19 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-// TODO: correctly handle these legacy defines
-#define MOSI    11
-#define MISO    12
-#define SCK     13
-#define SS      10
-#define SDA     0
-#define SCL     0
+// SPI and I2C (Wire) pins; indices match digital-pin-gpios
+#define MOSI    D11 /* P1.01 */
+#define MISO    D12 /* P1.08 */
+#define SCK     D13 /* P0.13 */
+#define SS      D10 /* P1.02 */
+#define SDA     D18 /* P0.31 */
+#define SCL     D19 /* P0.02 */
+
+// RGB LED and power (indices match digital-pin-gpios)
+#define LEDR    D22 /* P0.24 */
+#define LEDG    D23 /* P0.16 */
+#define LEDB    D24 /* P0.06 */
+#define LED_PWR D25 /* P1.09 */
 
 /* Arduino_APDS9960 library */
 #define PIN_INT_APDS 26 /* D26 - P0.19 */
